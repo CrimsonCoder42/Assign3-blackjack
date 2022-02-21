7adf980994ca14148b73ce92dd8af54bde464001
Nostro37 <nostro37@gmail.com>
2022-02-21 18:15:59 -0500

    buttons adjusted

---
+++ app.js
@@ -0,0 +1,11 @@
+const hotdogbtn = document.getElementById('hotdogs');
+const friesbtn = document.getElementById('fries');
+const sodabtn = document.getElementById('soda');
+const sauerkrautbtn = document.getElementById('sauerkraut');
+const 
+const allbuttons = document.querySelectorAll('.item');
+
+
+
+
+
