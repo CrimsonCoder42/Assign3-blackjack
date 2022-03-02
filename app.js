8677ed5da4d32073dc4f23d352a715570cdb1e1c
Nostro37 <nostro37@gmail.com>
2022-03-01 20:37:30 -0500

    added money button

--- app.js
+++ app.js
@@ -1,6 +1,8 @@
 import { shuffledDeck } from './cardDeck.js';
 
-console.log(shuffledDeck)
+let playerMoney = 0;
+let computerMoney;
+let money;
 
 const computerHide = document.getElementById('computer-hide');
 const computerShow = document.getElementById('computer-show');
@@ -9,5 +11,25 @@ const pot = document.getElementById('pot');
 const playerCash = document.getElementById('playerCash');
 const hitBtn = document.getElementById('hit');
 const stayBtn = document.getElementById('hit');
+const addBtn = document.getElementById('addMoney');
 const playerCard1 = document.getElementById('player-card1');
 const playerCard2 = document.getElementById('player-card2');
+getMoney()
+
+function getMoney() {
+    //money = parseInt(prompt('How much would you like to add to your balance?'))
+    if(money > 0) {
+        start()
+    } else {
+        //alert("Must be a number more than 0!")
+    }
+}
+
+function start() {
+    console.log(money)
+    playerMoney += money; 
+    console.log(playerMoney)
+    computerMoney = money;
+    computerCash.innerHTML = `computer $$$ ${computerMoney}`;
+    playerCash.innerHTML = `Player1 $$$ ${playerMoney}`;
+}
