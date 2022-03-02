e320cd6d648a2a676301bee1bef4ef4b66a000da
Nostro37 <nostro37@gmail.com>
2022-03-02 12:10:29 -0500

    initial money setup done

--- app.js
+++ app.js
@@ -3,35 +3,37 @@ import { shuffledDeck } from './cardDeck.js';
 let playerMoney = 0;
 let computerMoney;
 
-const money = document.querySelector('input').value;
 const computerHide = document.getElementById('computer-hide');
 const computerShow = document.getElementById('computer-show');
-const computerCash = document.getElementById('computerCash').innerHTML;
+const computerCash = document.getElementById('computerCash');
 const pot = document.getElementById('pot');
-const playerCash = document.getElementById('playerCash').innerHTML;
+const playerCash = document.getElementById('playerCash');
 const hitBtn = document.getElementById('hit');
-const stayBtn = document.getElementById('hit');
 const addBtn = document.getElementById('addMoney');
+const stayBtn = document.getElementById('hit');
 const playerCard1 = document.getElementById('player-card1');
 const playerCard2 = document.getElementById('player-card2');
+let money = document.getElementById('moneyAmount');
 
 addBtn.addEventListener('click', getMoney)
 
 
 function getMoney() {
-    //money = parseInt(prompt('How much would you like to add to your balance?'))
-    if(money > 0) {
+    let addedMoney = parseInt(money.value);
+    if(addedMoney > 0) {
+        playerMoney += addedMoney;
+        playerCash.innerHTML = `Player $$$ ${playerMoney}`;
+        computerMoney = 1000;
+        computerCash.innerHTML = `computer $$$ ${computerMoney}`;
+        addBtn.classList.add('hidden');
+        money.classList.add('hidden');
         start()
     } else {
-        //alert("Must be a number more than 0!")
+        alert("Must be a NUMBER & more than 0!")
     }
 }
 
 function start() {
-    console.log(money)
-    playerMoney += money; 
-    console.log(playerMoney)
-    computerMoney = money;
-    computerCash = `computer $$$ ${computerMoney}`;
-    playerCash = `Player1 $$$ ${playerMoney}`;
+    
+    
 }
