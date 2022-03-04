2508a36be6426090774447d87082ff85817a6572
Nostro37 <nostro37@gmail.com>
2022-03-03 22:07:58 -0500

    refactoring code v1

--- app.js
+++ app.js
@@ -1,37 +1,39 @@
-import { shuffledDeck } from './cardDeck.js';
+import {
+    shuffledDeck
+} from './cardDeck.js';
 
 let playerMoney = 0;
 let betAdded = 0;
 let computerMoney;
-let computerCount = [];
-let playerCount = [];
-let cardsUsed = [];
+let compScore = 0;
+let playScore = 0;
 
-const betAmount = document.getElementById('betAmount');
 const betBtn = document.getElementById('betBtn');
 const computerCash = document.getElementById('compMoney');
 const pot = document.getElementById('pot');
 const playerCash = document.getElementById('playerMoney');
 const hitBtn = document.getElementById('hit');
 const addBtn = document.getElementById('addMoneyBtn');
-const stayBtn = document.getElementById('hit');
-let money = document.getElementById('moneyAmount');
-let cardWorth1 = document.getElementsByClassName("cardWorth1 playCardW1");
+const stayBtn = document.getElementById('stay');
+const playMoney = document.getElementById('playMoney');
 
 
-addBtn.addEventListener('click', getMoney)
-betBtn.addEventListener('click', playerBet)
+addBtn.addEventListener('click', getMoney);
+betBtn.addEventListener('click', playerBet);
+stayBtn.addEventListener('click', stay);
 
 
 function getMoney() {
+    let money = document.getElementById('moneyAmount');
     let addedMoney = parseInt(money.value);
-    if(addedMoney > 0) {
+    if (addedMoney > 0) {
         playerMoney += addedMoney;
         playerCash.innerHTML = `Player $$$ ${playerMoney}`;
         computerMoney = addedMoney;
         computerCash.innerHTML = `Computer $$$ ${computerMoney}`;
         addBtn.className = "hidden";
         money.className = "hidden";
+        playMoney.className = "hidden";
         start()
     } else {
         alert("Must be a NUMBER & more than 0!")
@@ -39,13 +41,18 @@ function getMoney() {
 }
 
 function start() {
+    betAmount.className = 'betAmt'
+    betBtn.className = 'betAmt'
+    hitBtn.className = 'btn'
+    stayBtn.className = 'btn'
     dealCards()
 }
 
 function playerBet() {
-    let betPlaced = parseInt(betAmount.value); 
+    const betAmount = document.getElementById('betAmount');
+    let betPlaced = parseInt(betAmount.value);
     console.log(betPlaced)
-    if(betPlaced > 0){
+    if (betPlaced > 0) {
         betAdded += betPlaced;
         pot.innerHTML = `POT $$$ ${betAdded}`;
         playerMoney -= betPlaced;
@@ -53,43 +60,56 @@ function playerBet() {
     } else {
         alert("Must be a NUMBER & more than 0!");
     }
-  
+
 }
 
 function dealCards() {
-    const computerHide = document.getElementById('compCard1');
-    const computerShow = document.getElementById('compCard2');
-    const playerCard1 = document.getElementById('playerCard1');
-    const playerCard2 = document.getElementById('playerCard2');
+    const comp1 = document.getElementById('compCard1');
+    const comp2 = document.getElementById('compCard2');
+    const play1 = document.getElementById('playerCard1');
+    const play2 = document.getElementById('playerCard2');
+    let cardArray = [comp1, comp2, play1, play2]
+    comp1.className = "computer-hide";
 
-    computerCount = []
-    playerCount = []
-    if (cardsUsed.length < 52) {
-        for(let i = 0; i < 2; i++){
-            computerCount.push(shuffledDeck[Math.floor(Math.random() * 53)]);
-            playerCount.push(shuffledDeck[Math.floor(Math.random() * 53)]);
-        }
+    for (let i = 1; i < 5; i++) {
+
+        let card = shuffledDeck[Math.floor(Math.random() * 53)];
+        const {
+            color,
+            suit,
+            worth
+        } = card;
+
+        cardDisplay(color, suit, worth, cardArray[i])       
     }
-    computerShow.className = computerCount[1].suit === "♣" || computerCount[0].suit === "♠" ? "card black" : "card red";
-    computerShow.innerHTML = computerCount[1].suit 
-    computerShow.dataset.number = computerCount[1].worth
-
-    playerCard1.className = playerCount[0].suit === "♣" || playerCount[0].suit === "♠" ? "card black" : "card red";
-    playerCard1.innerHTML = playerCount[0].suit 
-    playerCard1.dataset.number = playerCount[0].worth
-
-    playerCard2.className = playerCount[1].suit === "♣" || playerCount[1].suit === "♠" ? "card black" : "card red";
-    playerCard2.innerHTML = playerCount[1].suit 
-    playerCard2.dataset.number = playerCount[1].worth
-
-    computerHide.className = "computer-hide";
-
-    console.log(computerCount[0].suit);
-    console.log(computerCount[0].worth);
 }
 
-console.log(cardsUsed);
+function cardDisplay(color, suit, worth, card) {
+    card.className = `card ${color}`;
+    card.innerHTML = suit;
+    card.dataset.number = worth;
+    // if ((num < 3) && (worth !== "J" || worth !== "Q" || worth !== "K" || worth !== "A" )){
+    //     compScore += parseInt(worth);
+    //     console.log(compScore)
+    // } else if ((num < 3) && (worth === "J" || worth === "Q" || worth === "K")){
+    //     compScore += parseInt(worth);
+    //     console.log(compScore)
+    // } else if ((num < 3)) {
+    //     console.log('boom!')
+    //}
+}
+
+function score() {
+    
+}
 
 
 
 
+function checkTwentyOnePlus() {
+    let playerhand = playerCount.forEach(ele => {
+        if (ele.worth === "J" || ele.worth === "Q" || ele.worth === "Q") {
+
+        }
+    })
+}
